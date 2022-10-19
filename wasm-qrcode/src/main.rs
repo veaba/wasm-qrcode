99d0bddf4750e1f6dceb83568a15c942853490cd
veaba <godpu@outlook.com>
2022-10-19 22:15:49 +0800

    chore: add impl example

--- wasm-qrcode/src/main.rs
+++ wasm-qrcode/src/main.rs
@@ -1,20 +1,10 @@
-use crate::rs_block::RsBlockFns;
+use crate::rs_block::Tweet;
 
-// mod rs_block;
-// mod shared;
 mod rs_block;
 
-// use crate::block::{a};
-// use crate::shared::QRErrorCorrectLevel;
-
 fn main() {
-    // println!(" ============ {:}", QRErrorCorrectLevel::H);
-
-    const G15: i32 = (1 << 10) | (1 << 8) | (1 << 5) | (1 << 4) | (1 << 2) | (1 << 1) | (1 << 0);
-
-    println!(" 与或非操作= {}", G15);
-    // <RSBlock2 as Trait>::new
-    let bar = RsBlockFns::get_rs_blocks(&());
-    // let bar = RSBlock2::ab();
-    println!(" rs_block_class 私有属性= {:?}", bar);
+    let code = Tweet {
+        username: String::from("Alice")
+    };
+    println!("code=>{}", code.get_name())
 }
--- wasm-qrcode/src/rs_block.rs
+++ wasm-qrcode/src/rs_block.rs
@@ -1,31 +1,9 @@
-pub fn a1() -> &'static str {
-    println!("a1");
-    "a1"
+pub struct Tweet {
+    pub username: String,
 }
 
-// impl RSBlock2 {
-pub fn ab() -> &'static str {
-    println!("222");
-    "ddd"
-}
-
-pub(crate) struct StructRsBlock {
-    total_count: i32,
-    data_count: i32,
-}
-
-// 相当于 interface
-pub(crate) trait RsBlockFns {
-    fn get_rs_blocks(&self) -> &'static str;
-    fn get_rs_block_table(&self) {}
-}
-
-impl RsBlockFns for StructRsBlock {
-    fn get_rs_blocks(&self) -> &'static str {
-        println!("222");
-        "ddd"
+impl Tweet {
+    pub fn get_name(&self) -> String {
+        format!("{}", self.username)
     }
-    fn get_rs_block_table(&self) {}
 }
-
-// 某个结构体实现特质(trait), 需要用 impl for
